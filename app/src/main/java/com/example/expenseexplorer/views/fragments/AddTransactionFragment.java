7e76da61866cec6cbbaee28cd14d10d24514dabf
amandeep-patro <amandeep.patro04@gmail.com>
2023-10-15 08:46:38 +0530

    first

---
+++ app/src/main/java/com/example/expenseexplorer/views/fragments/AddTransactionFragment.java
@@ -0,0 +1,226 @@
+package com.example.expenseexplorer.views.fragments;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.expenseexplorer.R;
+import com.example.expenseexplorer.adapters.AccountsAdapter;
+import com.example.expenseexplorer.adapters.CategoryAdapter;
+import com.example.expenseexplorer.databinding.FragmentAddTransactionBinding;
+import com.example.expenseexplorer.databinding.ListDialogBinding;
+import com.example.expenseexplorer.models.Account;
+import com.example.expenseexplorer.models.Category;
+import com.example.expenseexplorer.models.Transaction;
+import com.example.expenseexplorer.utils.Constants;
+import com.example.expenseexplorer.utils.Helper;
+import com.example.expenseexplorer.views.activites.MainActivity;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class AddTransactionFragment extends BottomSheetDialogFragment implements PaymentResultListener {
+
+    public AddTransactionFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    FragmentAddTransactionBinding binding;
+    Transaction transaction;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAddTransactionBinding.inflate(inflater);
+
+
+        transaction = new Transaction();
+
+        binding.incomeBtn.setOnClickListener(view -> {
+            binding.incomeBtn.setBackground(getContext().getDrawable(R.drawable.income_selector));
+            binding.expenseBtn.setBackground(getContext().getDrawable(R.drawable.default_selector));
+            binding.expenseBtn.setTextColor(getContext().getColor(R.color.textColor));
+            binding.incomeBtn.setTextColor(getContext().getColor(R.color.greenColor));
+
+            transaction.setType(Constants.INCOME);
+        });
+
+        binding.expenseBtn.setOnClickListener(view -> {
+            binding.incomeBtn.setBackground(getContext().getDrawable(R.drawable.default_selector));
+            binding.expenseBtn.setBackground(getContext().getDrawable(R.drawable.expense_selector));
+            binding.incomeBtn.setTextColor(getContext().getColor(R.color.textColor));
+            binding.expenseBtn.setTextColor(getContext().getColor(R.color.redColor));
+
+            transaction.setType(Constants.EXPENSE);
+        });
+
+        binding.date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext());
+                datePickerDialog.setOnDateSetListener((datePicker, i, i1, i2) -> {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.set(Calendar.DAY_OF_MONTH, datePicker.getDayOfMonth());
+                    calendar.set(Calendar.MONTH, datePicker.getMonth());
+                    calendar.set(Calendar.YEAR, datePicker.getYear());
+
+                    //SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM, yyyy");
+                    String dateToShow = Helper.formatDate(calendar.getTime());
+
+                    binding.date.setText(dateToShow);
+
+                    transaction.setDate(calendar.getTime());
+                    transaction.setId(calendar.getTime().getTime());
+                });
+                datePickerDialog.show();
+            }
+        });
+
+        binding.category.setOnClickListener(c-> {
+            ListDialogBinding dialogBinding = ListDialogBinding.inflate(inflater);
+            AlertDialog categoryDialog = new AlertDialog.Builder(getContext()).create();
+            categoryDialog.setView(dialogBinding.getRoot());
+
+
+
+            CategoryAdapter categoryAdapter = new CategoryAdapter(getContext(), Constants.categories, new CategoryAdapter.CategoryClickListener() {
+                @Override
+                public void onCategoryClicked(Category category) {
+                    binding.category.setText(category.getCategoryName());
+                    transaction.setCategory(category.getCategoryName());
+                    categoryDialog.dismiss();
+                }
+            });
+            dialogBinding.recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
+            dialogBinding.recyclerView.setAdapter(categoryAdapter);
+
+            categoryDialog.show();
+        });
+
+        binding.account.setOnClickListener(c-> {
+            ListDialogBinding dialogBinding = ListDialogBinding.inflate(inflater);
+            AlertDialog accountsDialog = new AlertDialog.Builder(getContext()).create();
+            accountsDialog.setView(dialogBinding.getRoot());
+
+            ArrayList<Account> accounts = new ArrayList<>();
+            accounts.add(new Account(0, "Cash"));
+            accounts.add(new Account(0, "Online"));
+            accounts.add(new Account(0, "Other"));
+
+            AccountsAdapter adapter = new AccountsAdapter(getContext(), accounts, new AccountsAdapter.AccountsClickListener() {
+                @Override
+                public void onAccountSelected(Account account) {
+                    binding.account.setText(account.getAccountName());
+                    transaction.setAccount(account.getAccountName());
+                    accountsDialog.dismiss();
+                }
+            });
+            dialogBinding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            //dialogBinding.recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+            dialogBinding.recyclerView.setAdapter(adapter);
+
+            accountsDialog.show();
+
+        });
+
+        binding.saveTransactionBtn.setOnClickListener(c-> {
+            double amount = Double.parseDouble(binding.amount.getText().toString());
+            String note = binding.note.getText().toString();
+
+            if(transaction.getType().equals(Constants.EXPENSE)) {
+                transaction.setAmount(amount*-1);
+            } else {
+                transaction.setAmount(amount);
+            }
+
+            transaction.setNote(note);
+
+            if(transaction.getAccount()=="Online" && transaction.getType().equals(Constants.EXPENSE)){
+                startPayment(amount);
+            }
+
+            ((MainActivity)getActivity()).viewModel.addTransaction(transaction);
+            ((MainActivity)getActivity()).getTransactions();
+            dismiss();
+        });
+
+        return binding.getRoot();
+    }
+
+    private void startPayment(double amount){
+
+        /**
+         * Instantiate Checkout
+         */
+        Checkout checkout = new Checkout();
+        checkout.setKeyID("rzp_test_0tYQwB82yYC3G7");
+        /**
+         * Set your logo here
+         */
+        checkout.setImage(R.drawable.ic_launcher_foreground);
+
+        /**
+         * Reference to current activity
+         */
+        final Activity activity = getActivity();
+
+        /**
+         * Pass your payment options to the Razorpay Checkout as a JSONObject
+         */
+        try {
+            JSONObject options = new JSONObject();
+
+            options.put("name", "amandeepPatro");
+            options.put("description", "Reference No. #123456");
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+//            options.put("order_id", "order_DBJOWzybf0sJbb");//from response of step 3.
+            options.put("theme.color", "#3399cc");
+            options.put("currency", "INR");
+            options.put("amount", amount*100);//pass amount in currency subunits
+            options.put("prefill.email", " ");
+            options.put("prefill.contact"," ");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            options.put("retry", retryObj);
+
+            checkout.open(activity, options);
+
+        } catch(Exception e) {
+            Log.e("TAG", "Error in starting Razorpay Checkout", e);
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(getContext(), "success" + s, Toast.LENGTH_SHORT).show();
+        Log.d("ONSUCCESS", "onPaymentSuccess: " + s);
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(getContext(), "failure" + s, Toast.LENGTH_SHORT).show();
+        Log.d("ONERROR", "onPaymentError: "+s);
+    }
+
+}
